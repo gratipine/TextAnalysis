03d16fc4e59e4ecf625112f58bed2cf3026ea848
gratipine <gratipine@protonmail.com>
2019-09-16 21:05:39 +0100

    Init

---
+++ 1 Practice.R
@@ -0,0 +1,82 @@
+library(dplyr)
+library(tidytext)
+library(gutenbergr)
+library(ggplot2)
+library(purrr)
+library(data.table)
+
+# Hoping to find some Agatha Christie books and analyze them
+authors_dt <- data.table(gutenberg_authors)
+authors_dt[author == "Christie, Agatha"]
+
+# pull all books written by her
+gutenberg_metadata_dt <- data.table(gutenberg_metadata)
+gutenberg_metadata_dt[author == "Christie, Agatha"]
+
+# A bit of a dissappointment - only two are found. Let's see what they have
+ids_christie_books <- gutenberg_metadata_dt[
+  author == "Christie, Agatha",
+  gutenberg_id]
+
+books <- gutenberg_download(ids_christie_books)
+
+
+# Do a simple count of the words. Should be in only edge cases that we
+# worry about chapter, part and so on
+
+christie_words <- books %>%
+  unnest_tokens(word, text) %>%
+  anti_join(stop_words)
+
+christie_words <- merge(
+  christie_words, 
+  gutenberg_metadata_dt[author == "Christie, Agatha", .(gutenberg_id, title)],
+  by =  "gutenberg_id")
+
+conted_words <- christie_words %>%
+  count(word, sort = TRUE)
+
+# A handy reminder that while The Secret Adversary was a Tommy and Tuppence 
+# book, The Mysterious Affair at Styles was a Poirot book. Hopefully by the end 
+# of this analysis I actually remember what they were about.
+counted_words_by_book <- christie_words %>%
+  group_by(title) %>%
+  count(word, sort = TRUE)
+
+# Should probably sort out the ' words - that's is not necessary
+christie_words %>%
+  count(word, sort = TRUE) %>%
+  filter(n > 100) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(word, n)) +
+  geom_col() +
+  xlab(NULL) +
+  coord_flip()
+
+frequencies <- christie_words %>%
+  mutate(word = stringr::str_extract(word, "[a-z']+")) %>%
+  group_by(title) %>%
+  count(word) %>%
+  mutate(proportion = n / sum(n)) %>% 
+  select(-n) %>%
+  tidyr::spread(title, proportion) %>% 
+  tidyr::gather(title, proportion, `The Mysterious Affair at Styles`)
+
+# Make the great plot
+library(scales)
+ggplot(frequencies, 
+       aes(x = proportion, y = `The Secret Adversary`, 
+           color = abs(`The Secret Adversary` - proportion))) +
+  geom_abline(color = "gray40", lty = 2) +
+  geom_jitter(alpha = 0.1, size = 2.5, width = 0.3, height = 0.3) +
+  geom_text(aes(label = word), check_overlap = TRUE, vjust = 1.5) +
+  scale_x_log10(labels = percent_format()) +
+  scale_y_log10(labels = percent_format()) +
+  scale_color_gradient(
+    limits = c(0, 0.001), low = "darkslategray4", high = "gray75") +
+  theme(legend.position="none") +
+  labs(y = "The Secret Adversary", x = "The Mysterious Affair at Styles")
+
+# Nothing jogs my memory, but I was wrong about "chapter" not mattering that 
+# much - it is right in the middle and very common for both books, even though 
+# it should not be that common. 
---
+++ 1 The tidy text format.R
@@ -0,0 +1,157 @@
+# Beginnings of Tokenization
+
+poem <- c("Because I could not stop for Death -",
+          "He kindly stopped for me -",
+          "The Carriage held but just Ourselves -",
+          "and Immortality")
+
+poem
+
+
+library(dplyr)
+text_df <- tibble(line = 1:4, text = poem)
+
+text_df
+
+library(tidytext)
+
+text_df %>%
+  unnest_tokens(word, text)
+
+
+# Analysis of the Jane Austin works
+
+library(janeaustenr)
+library(dplyr)
+library(stringr)
+
+original_books <- austen_books() %>%
+  group_by(book) %>%
+  mutate(linenumber = row_number(),
+         chapter = cumsum(str_detect(text, regex("^chapter [\\divxlc]",
+                                                 ignore_case = TRUE)))) %>%
+  ungroup()
+
+original_books
+
+tidy_books <- original_books %>%
+  unnest_tokens(word, text)
+
+tidy_books
+
+
+# Removing stopwords
+data(stop_words)
+
+tidy_books <- tidy_books %>%
+  anti_join(stop_words)
+
+tidy_books
+
+# Perhaps we are not interested in the numbers following chapters as well?
+
+
+# Most common words in the books
+tidy_books %>%
+  count(word, sort = TRUE) 
+
+# Perhaps make a word cloud out of that data frame?
+
+library(ggplot2)
+
+tidy_books %>%
+  count(word, sort = TRUE) %>%
+  filter(n > 600) %>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot(aes(word, n)) +
+  geom_col() +
+  xlab(NULL) +
+  coord_flip()
+
+
+# Word frequencies -----
+
+library(gutenbergr)
+
+# Download using the Project Gutenberg ids for the following books:
+# - The Time Machine
+# - The War of the Worlds
+# - The Invisible Man
+# - The Island of Doctor Moreau
+
+
+hgwells <- gutenberg_download(c(35, 36, 5230, 159))
+
+tidy_hgwells <- hgwells %>%
+  unnest_tokens(word, text) %>%
+  anti_join(stop_words)
+
+# Grouping by book id yields interesting results - The time machine has time 
+# mentioned in it 200 times, while in the other ones it is mentioned a 
+# collective 254 times. I would have expected more mentions in the first one, 
+# for some reason. However, given the distribution, perhaps it is the expected 
+# amount of times.
+
+tidy_hgwells %>%
+  count(word, sort = TRUE)
+
+tidy_hgwells %>%
+  group_by(gutenberg_id) %>%
+  count(word, sort = TRUE)
+
+# Similar analysis with the Bronte books
+# - Jane Eyre
+# - Wuthering Heights
+# - The Tenant of Wildfell Hall
+# - Villette
+# - Agnes Grey
+
+bronte <- gutenberg_download(c(1260, 768, 969, 9182, 767))
+
+tidy_bronte <- bronte %>%
+  unnest_tokens(word, text) %>%
+  anti_join(stop_words)
+
+# Interestingly enough, the Bronte sisters and H.G. Wells all talk a lot about 
+# time.
+tidy_bronte %>%
+  count(word, sort = TRUE)
+
+# Gather all the authors together
+library(tidyr)
+
+frequency <- bind_rows(mutate(tidy_bronte, author = "Brontë Sisters"),
+                       mutate(tidy_hgwells, author = "H.G. Wells"), 
+                       mutate(tidy_books, author = "Jane Austen")) %>% 
+  mutate(word = str_extract(word, "[a-z']+")) %>%
+  count(author, word) %>%
+  group_by(author) %>%
+  mutate(proportion = n / sum(n)) %>% 
+  select(-n) %>% 
+  spread(author, proportion) %>% 
+  gather(author, proportion, `Brontë Sisters`:`H.G. Wells`)
+
+library(scales)
+
+# expect a warning about rows with missing values being removed
+ggplot(frequency, 
+       aes(x = proportion, y = `Jane Austen`, 
+           color = abs(`Jane Austen` - proportion))) +
+  geom_abline(color = "gray40", lty = 2) +
+  geom_jitter(alpha = 0.1, size = 2.5, width = 0.3, height = 0.3) +
+  geom_text(aes(label = word), check_overlap = TRUE, vjust = 1.5) +
+  scale_x_log10(labels = percent_format()) +
+  scale_y_log10(labels = percent_format()) +
+  scale_color_gradient(
+    limits = c(0, 0.001), low = "darkslategray4", high = "gray75") +
+  facet_wrap(~author, ncol = 2) +
+  theme(legend.position="none") +
+  labs(y = "Jane Austen", x = NULL)
+
+
+# Correlations between the two sets 
+cor.test(data = frequency[frequency$author == "Brontë Sisters",],
+         ~ proportion + `Jane Austen`)
+
+cor.test(data = frequency[frequency$author == "H.G. Wells",], 
+         ~ proportion + `Jane Austen`)
