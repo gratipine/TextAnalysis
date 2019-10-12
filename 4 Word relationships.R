dd184554488cd0a2f9e431efc8b43449666a4b1e
gratipine <gratipine@protonmail.com>
2019-10-12 20:49:54 +0100

    Reading chapter 4

--- 4 Word relationships.R
+++ 4 Word relationships.R
@@ -124,3 +124,44 @@ negated_words %>%
   facet_wrap(~word1, scales = "free") +
   coord_flip()
 
+# Visualizing a network of bigrams using ggraph -------------------
+library(igraph)
+
+# filter for only relatively common combinations
+bigram_graph <- bigram_counts %>%
+  filter(n > 20) %>%
+  graph_from_data_frame()
+
+bigram_graph
+
+library(ggraph)
+set.seed(2017)
+
+ggraph(bigram_graph, layout = "fr") +
+  geom_edge_link() +
+  geom_node_point() +
+  geom_node_text(aes(label = name), vjust = 1, hjust = 1)
+
+set.seed(2016)
+
+a <- grid::arrow(type = "closed", length = unit(.15, "inches"))
+
+ggraph(bigram_graph, layout = "fr") +
+  geom_edge_link(aes(edge_alpha = n), show.legend = FALSE,
+                 arrow = a, end_cap = circle(.07, 'inches')) +
+  geom_node_point(color = "lightblue", size = 5) +
+  geom_node_text(aes(label = name), vjust = 1, hjust = 1) +
+  theme_void()
+
+# Visualizing bigrams in other texts --------------------------------------
+library(dplyr)
+library(tidyr)
+library(tidytext)
+library(ggplot2)
+library(igraph)
+library(ggraph)
+
+count_bigrams <- function(dataset){
+  dataset %>%
+    unnest_tokens(bigram, text, )
+}
